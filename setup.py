8d76bd20bf0ff2293d988d1fa216deb9c725eafa
Wouter Bolsterlee <uws@xs4all.nl>
2013-10-24 21:50:53 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='telwoord',
+    description="Convert numerals into their Dutch written form",
+    version='0.1dev',
+    author="Wouter Bolsterlee",
+    author_email="uws@xs4all.nl",
+    py_modules=['telwoord'],
+    url='https://github.com/wbolster/telwoord',
+    license="BSD",
+)
