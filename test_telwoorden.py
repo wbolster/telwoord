7be7fc29960360616ffde7a339b2209b1c1c95e1
Wouter Bolsterlee <uws@xs4all.nl>
2013-10-24 22:57:42 +0200

    Add more tests (many are failing)

--- test_telwoorden.py
+++ test_telwoorden.py
@@ -11,18 +11,33 @@ def test_cardinal():
         # Normal numbers
         0: "nul",
         1: "een",
+        11: "elf",
+        19: "negentien",
+        20: "twintig",
+        100: "honderd",
+        200: "tweehonderd",
+        999: "negenhonderdnegenennegentig",
+        1000: "duizend",
+        12001: "twaalfduizend een",
+        99999: "negenennegentigduizend negenhonderdnegenennegentig",
+
+        # From http://woordenlijst.org/leidraad/6/9/
         2: "twee",
         20: "twintig",
         22: "tweeëntwintig",
-        100: "honderd",
         200: "tweehonderd",
         222: "tweehonderdtweeëntwintig",
-        999: "negenhonderdnegenennegentig",
-        # 1000: "duizend",
-        # 2200: "tweeëntwintighonderd",
-        # 2220: "tweeduizend tweehonderdtwintig",
-        # 2220222: "twee miljoen tweehonderdtwintigduizend "
-        #          "tweehonderdtweeëntwintig",
+        2200: "tweeëntwintighonderd",
+        2220: "tweeduizend tweehonderdtwintig",
+        2220222: "twee miljoen tweehonderdtwintigduizend "
+                 "tweehonderdtweeëntwintig",
+
+        # From https://onzetaal.nl/taaladvies/advies/getallen-uitschrijven
+        108: "honderdacht",
+        678: "zeshonderdachtenzeventig",
+        2013: "tweeduizend dertien",
+        2577: "tweeduizend vijfhonderdzevenenzeventig",
+        17053980: "zeventien miljoen drieënvijftigduizend negenhonderdtachtig",
 
         # Negative numbers
         -1: "min een",
